6b181a3a1e30caedd52ba4cb6a71ab9512563561
Samson Momanyi <samsonmomanyi2005@gmail.com>
2026-02-24 21:53:33 +0300

    rename WeatherInfoCommand entries to PascalCase

--- composeApp/src/commonMain/kotlin/org/example/project/weather/presentation/weatherInfo/WeatherInfoCommand.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/weather/presentation/weatherInfo/WeatherInfoCommand.kt
@@ -2,12 +2,10 @@ package org.example.project.weather.presentation.weatherInfo
 
 sealed interface WeatherInfoCommand {
 
-    data class onSearchBarValChange(val queryState : SearchQueryInteractionState ) : WeatherInfoCommand
-    data class onDaySelected(val selectedIndex : Int) : WeatherInfoCommand
+    // FIX: renamed to PascalCase — Kotlin objects/classes in sealed interfaces should be PascalCase
+    data class OnSearchBarValChange(val queryState: SearchQueryInteractionState) : WeatherInfoCommand
+    data class OnDaySelected(val selectedIndex: Int) : WeatherInfoCommand
 
-
-
-    object adjustSearchBar : WeatherInfoCommand
-    object openLocationSheet : WeatherInfoCommand
-
+    object AdjustSearchBar : WeatherInfoCommand
+    object OpenLocationSheet : WeatherInfoCommand
 }
